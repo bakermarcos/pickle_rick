a718a533c68dfb888f92bd547d700a9791341a74
Marcos Baker <bakermarcos52@gmail.com>
2025-08-08 09:48:51 -0300

    fix unused

--- lib/core/shared/extensions/string_extension.dart
+++ lib/core/shared/extensions/string_extension.dart
@@ -1,37 +1,4 @@
-import 'package:intl/intl.dart';
-
 extension StringExtension on String {
-  String get toPrecisionString {
-    num value = num.parse(this);
-    return value.toStringAsFixed(2);
-  }
-
-  String get toBrazillianCurrency {
-    try {
-      if (contains('R\$')) {
-        return this;
-      }
-      num value = num.parse(this);
-
-      final formatter = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
-
-      return formatter.format(value);
-    } catch (e) {
-      return this;
-    }
-  }
-
-  String? get prefixFromPhone {
-    final digits = replaceAll(RegExp(r'\D'), '');
-    if (digits.length >= 12 && digits.startsWith('55')) {
-      return digits.substring(2, 4);
-    }
-    if (digits.length >= 10) {
-      return digits.substring(0, 2);
-    }
-    return null;
-  }
-
   String get characterId {
     return substring(lastIndexOf('/') + 1);
   }
